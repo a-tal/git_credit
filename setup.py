fc7f33ef3e8b43b124dfd10d71fca147dc177911
a-tal <github@talsma.ca>
2014-05-15 18:28:00 -0700

    call it 0.0.5... properly marking deps.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PyTest(TestCommand):
 
 setup(
     name="git_credit",
-    version="0.0.4",
+    version="0.0.5",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     packages=["git_credit"],
